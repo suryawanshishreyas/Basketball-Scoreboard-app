8e6901a68790ecacd050ff4057b357543caab4be
suryawanshishreyas <suryawanshishreyas@gmail.com>
2024-07-31 22:47:51 +0530

    added a functionality where winner is logged

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-let scoreHome = document.getElementById('scoreHome');
-let scoreGuest = document.getElementById('scoreGuest');
+var scoreHome = document.getElementById('scoreHome');
+var scoreGuest = document.getElementById('scoreGuest');
 
 scoreHome.innerText=0;
 scoreGuest.innerText=0;
 
 function incHomeBy1(){
     scoreHome.textContent = 1 + Number(scoreHome.textContent);
+    decider();
     return scoreHome.textContent;
 }
 function incHomeBy2(){
     scoreHome.textContent = 2 + Number(scoreHome.textContent);
+    decider();
     return scoreHome.textContent;
 }
 function incHomeBy3(){
     scoreHome.textContent = 3 + Number(scoreHome.textContent);
+    decider();
     return scoreHome.textContent;
 }
 function incGuestBy1(){
     scoreGuest.textContent = 1 + Number(scoreGuest.textContent);
+    decider();
     return scoreGuest.textContent;
 }
 function incGuestBy2(){
     scoreGuest.textContent = 2 + Number(scoreGuest.textContent);
+    decider();
     return scoreGuest.textContent;
 }
 function incGuestBy3(){
     scoreGuest.textContent = 3 + Number(scoreGuest.textContent);
+    decider();
     return scoreGuest.textContent;
 }
 
-
-
+function decider(){
+    if(scoreHome.textContent > scoreGuest.textContent){
+        console.log('ScoreHome winner');
+    }
+    else if(scoreHome.textContent == scoreGuest.textContent){
+        console.log('Score Equal');
+    }
+    else{
+        console.log('ScoreGuest Winner');
+    }
+}
